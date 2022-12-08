978ad837a79387ee70a78091f3ccf1a7a7a0c222
yuzzuki <yuzzuki@bk.ru>
2022-12-08 13:19:00 +0600

    fourthCommit

--- prototype2-app/src/pages/AddNewProject/AddNewProject.jsx
+++ prototype2-app/src/pages/AddNewProject/AddNewProject.jsx
@@ -3,7 +3,14 @@ import './style.css';
 const AddNewProject = () => {
     return (
         <div className="addNewProject">
+            <div className="personalAreaUserInfo">
+                <div className="personalAreaUserInfo personalAreaUserInfo__box">
+                    <input type="text"></input>
+                </div>
+                <div className="personalAreaUserInfo personalAreaUserInfo__box">
 
+                </div>
+            </div>
         </div>
     );
 }
